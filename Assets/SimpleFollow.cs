fc226ac670a54ebc6f7b219383c69a5aa33e887b
myeong chul <10jade0070@gmail.com>
2018-12-04 16:35:46 +0900

    카메라 워크
    * 기동 시에 취득한 목표물의 위치 관계를 유지하도록 자기 자신의 위치를 조정하는 스크립트를 생성함.

---
+++ Assets/SimpleFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    Vector3 diff;
+    public GameObject target;
+    public float followSpeed;
+    
+    void Start()
+    {
+        diff = target.transform.position - transform.position; // 추적거리 계산
+    }
+
+    
+    void LateUpdate()
+    {
+        // 선형 보간 함수(Lerp)에 의한 유연한 움직임 구현
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.transform.position - diff,
+            Time.deltaTime * followSpeed
+            );
+    }
+}
