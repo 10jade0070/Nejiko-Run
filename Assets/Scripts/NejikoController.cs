3012ebab04a455d99df5f9c229a5c83d9704cffa
myeong chul <10jade0070@gmail.com>
2018-12-07 15:45:06 +0900

    데미지 처리
    1. 라이프 취득용 함수
    
    네지코의 현재 라이프 값을 반환한다. 나중에 게임 전체의 판정에 이용할 예정이므로 이 시점에서 취득용의 함수를 구현해둔다.
    
    2. 기절 판정
    
    적과 접촉했을 떄 설정되는 recoverTime이 0보다 클 때 혹은 life가 전부 소진되었을 때 기절상태로 인해 동작할 수 없다는 값(true)을 반환한다.
    
    3. 기절 시의 행동
    
    기절 시에는 Z 방향으로도 X 방향으로도 진행되지 않도록 moveDirection을 0으로 설정한다. 또한, 기절 상태를 회복시키기 위한 타이머 변수 recoverTime의 감산을 한다.
    
    4. 기절 시 입력 취소
    
    기절 시에는 입력된 커맨드를 무효로 하여 차선 변경이나 점프 처리 등을 하지 않도록 한다.
    
    5.CharacterController의 충돌 함수
    
    OnControllerColliderHit 함수는 CharacterController의 경우는 콜라이더에서 일반적으로 사용하는 OnTriggerEnter와 OnCollisionEnter 등의 함수가 아닌 OnControllerColliderHit 함수를 이용해야 한다.
    
    6. 히트 처리
    
    히트한 오브젝트의 Tag가 Robo인 경우에 데미지 처리를 한다. 구체적으로는 life를 감산처리한 후에 기절 상태가 회복할 때까지의 시간을 설정한다. 또한, Animator에 데미지가 발생되었다는 트리거를 전달하여 히트한 적을 삭제한다.

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -7,18 +7,33 @@ public class NejikoController : MonoBehaviour
     const int MinLane = -2;
     const int MaxLane = 2;
     const float LaneWidth = 1.0f;
+    const int DefaultLife = 3; // 생명력
+    const float StunDuration = 0.5f; // 스턴
 
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
     int targetLane;
+    int life = DefaultLife;
+    float recoverTime = 0.0f;
 
     public float gravity;
     public float speedZ;
     public float speedX; // 수평 방향 속도의 파라미터
     public float speedJump;
     public float accelerationZ; // 전진 가속도의 파라미터
+
+    //---1.라이프 취득용 함수
+    public int Life()
+    {
+        return life;
+    }
+    //---2.기절 판정
+    public bool IsStan()
+    {
+        return recoverTime > 0.0f || life <= 0;
+    }
 
     void Start()
     {
@@ -34,14 +49,25 @@ public class NejikoController : MonoBehaviour
         if (Input.GetKeyDown("right")) MoveToRight();
         if (Input.GetKeyDown("space")) Jump();
 
+        //---3. 기절 시의 행동
+        if (IsStan())
+        {
+            // 움직임을 기절 상태에서 복귀 카운터를 진행한다
+            moveDirection.x = 0.0f;
+            moveDirection.z = 0.0f;
+            recoverTime -= Time.deltaTime;
+        }
+        else
+        {        
         // 서서히 가속하여 Z방향으로 계속 전진시킨다
         float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime); // 전진 속도 계산
         moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
 
         // X 방향은 목표의 포지션까지의 차등 비율로 속도를 계산
         float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth; // 수직 이동의 계산
-        moveDirection.x = ratioX * speedX;        
-           
+        moveDirection.x = ratioX * speedX;
+        }
+
         // 중력만큼의 힘을 매 프레임에 추가
         moveDirection.y -= gravity * Time.deltaTime;
 
@@ -59,6 +85,8 @@ public class NejikoController : MonoBehaviour
     // 왼쪽 차선으로 이동을 시작
     public void MoveToLeft ()
     {
+        if (IsStan()) return; //---4. 기절 시 입력 취소
+
         if (controller.isGrounded && targetLane > MinLane) targetLane--;       // 목표 차선변경
     }
 
@@ -66,16 +94,41 @@ public class NejikoController : MonoBehaviour
     public void MoveToRight ()
     {
         if (controller.isGrounded && targetLane < MaxLane) targetLane++;       // 목표 차선변경 
+        if (IsStan()) return; //---4. 기절 시 입력 취소
     }
 
+    // 점프
     public void Jump ()
     {
+        if (IsStan()) return; //---4. 기절 시 입력 취소
         if (controller.isGrounded)
         {
             moveDirection.y = speedJump;
             // 점프 트리거를 설정
             animator.SetTrigger("jump");
         }
+    }
+
+    //---5. CharacterController 충돌 함수
+    // CharacterController에 충돌이 발생했을 때의 처리
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (IsStan()) return;
+
+        //---6. 히트 처리
+        if (hit.gameObject.tag == "Robo")
+        {
+            // 라이프를 줄이고 기절 상태로 전환
+            life--;
+            recoverTime = StunDuration;
+
+            // 데미지 트리거를 설정
+            animator.SetTrigger("damage");
+
+            // 히트한 오브젝트는 삭제
+            Destroy(hit.gameObject);
+        }
+        
     }
 }
     
