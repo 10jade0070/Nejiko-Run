481f8f307f6f03a2890b753f2880eefa1a2d8509
10jade0070 <41108323+10jade0070@users.noreply.github.com>
2018-12-08 15:46:32 +0900

    Stage디자인
    적 출현 장소의 지정
    
    1. 적 오브젝트 생성
    
    스크립트가 시작했을 때 Inspector 뷰에서 지정한 적 프리팹을 동일한 위치에 생성한다.
    
    2. 부모-자식 관계 설정
    
    생성한 적 오브젝트를 스테이지 팁의 자식으로 설정함으로써 스테이지 팁 제거 시에 자동으로 제거되도록 한다.
    
    3. 씬 기즈모 표시
    
    EnemyPoint 스크립트는 씬에서는 아무것도 표시되지 않는 오브젝트이기 때문에 기즈모를 표시한다. 여기서의 처리는 반투명의 공이나 프리팹의 이름에 따라 아이콘을 표시한다.

---
+++ Assets/Scripts/EnemyPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPoint : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    void Start()
+    {
+        //---1. 적 오브젝트 생성
+        // 프리팹을 동일 위치에 생성
+        GameObject go = (GameObject)Instantiate(
+        prefab,
+        Vector3.zero,
+        Quaternion.identity
+        );
+
+        //---2. 부-자식 관계 설정
+        // 함께 삭제되도록 생성한 적 오브젝트를 자식으로 설정
+        go.transform.SetParent(transform, false);
+
+    }
+
+    // 스테이지 편집 중이므로 씬에 기즈모를 표시
+    private void OnDrawGizmos()
+    {
+        // 기즈모의 아랫부분이 지면과 같은 높이가 되도록 오프셋을 설정
+        Vector3 offset = new Vector3(0, 0.5f, 0);
+
+        // 공을 표시
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position + offset, 0.5f);
+
+        // 프리팹명의 아이콘을 표시
+        if (prefab != null)
+            Gizmos.DrawIcon(transform.position + offset, prefab.name, true);
+    }
+
+}
+
